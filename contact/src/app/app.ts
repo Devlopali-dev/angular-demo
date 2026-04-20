350248b7b78ed9563027aa56faa2104e1f732059
jeffrey.hoarau <jeffrey.hoarau@gendarmerie.interieur.gouv.fr>
2026-04-20 17:05:38 +0200

    contact

--- contact/src/app/app.ts
+++ contact/src/app/app.ts
@@ -1,12 +1,23 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Contact } from './contact';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
+
 export class App {
-  protected readonly title = signal('contact');
+  contact: Contact = {
+    nom: 'Dupont',
+    prenom: 'Jean',
+    age: 30,
+    adresse: {
+      rue: '18 rue des champs',
+      codePostal: 75000,
+      ville: 'Paris'
+    },
+    profession: 'Dev junior'
+  }
 }
---
+++ contact/src/app/contact.ts
@@ -0,0 +1,9 @@
+import { Adresse } from './adresse';
+
+export interface Contact {
+  nom: string
+  prenom: string
+  age: number
+  adresse: Adresse
+  profession: string
+}
